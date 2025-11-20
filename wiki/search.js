c8217c84565daeb3b410d083d40ef5457dcd5d94
vuki-dev.exe <rockysemaileo@gmail.com>
2025-11-20 20:15:55 +0100

    Delete wiki directory

--- wiki/search.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementsByClassName('srcbutton');
-var input = 0;
-var output = 0;
-button[0].addEventListener('click',function testfunction(){
-    input = Array.from(document.querySelectorAll('.srcinput')).map(input => input.value);
-    output = "search.html?input=" + encodeURIComponent(input);
-    window.location.href = output;
-});
-
